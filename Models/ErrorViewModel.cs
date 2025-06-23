001825911d6c08111cbcb1ddd88c1621f7a8fa17
Isis <isis.bea@gmail.com>
2025-06-23 13:10:12 -0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebGatoMia.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
